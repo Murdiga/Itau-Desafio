78a9eb510b8f2e7b4364cb4249580b3bfb596682
Guilherme <guimurdiga@hotmail.com>
2026-03-31 22:16:52 -0300

    refactor: implement BusinessException handling in GlobalExceptionHandler

--- src/main/java/br/com/itau/desafio/business/exception/globalexceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/br/com/itau/desafio/business/exception/globalexceptionhandler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import br.com.itau.desafio.business.exception.businessexception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,9 +15,18 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handlerException(Exception e){
 
-        log.error("Error >>> ", e);
+        log.error("ERROR >>> ", e);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<Object> handlerBusinessException(BusinessException e){
+        
+        log.error("ERROR >>> ", e);
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(e.getMessage());
 
     }
 
